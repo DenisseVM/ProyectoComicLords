b45bf076cbf122950296053e1cdc9fc40efa6e6b
fabi- <fabi-rayo@hotmail.es>
2022-11-19 20:49:13 -0300

    clase producto

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -10,4 +10,72 @@ package modelo;
  */
 public class Producto {
     
+    int codigo,precio,stock;
+    String nombre,editorial,autor;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, int precio, int stock, String nombre, String editorial, String autor) {
+        this.codigo = codigo;
+        this.precio = precio;
+        this.stock = stock;
+        this.nombre = nombre;
+        this.editorial = editorial;
+        this.autor = autor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", precio=" + precio + ", stock=" + stock + ", nombre=" + nombre + ", editorial=" + editorial + ", autor=" + autor + '}';
+    }
+    
 }
