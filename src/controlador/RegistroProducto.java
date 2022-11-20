1dccddebe4320c82f3afcff23bc0e0648711f63a
fabi- <fabi-rayo@hotmail.es>
2022-11-19 21:20:37 -0300

    clase producto buascar tod

--- src/controlador/RegistroProducto.java
+++ src/controlador/RegistroProducto.java
@@ -5,9 +5,13 @@
 package controlador;
 
 import bdd.Conexion;
+import java.awt.HeadlessException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import modelo.Producto;
 
 /**
@@ -40,6 +44,44 @@ public class RegistroProducto {
             System.out.println("Error en la consulta SQL al agregar un producto " + e.getMessage());
             return false;
         }
+    }
+        
+            public List<Producto> buscarTodos() {
+
+        List<Producto> lista = new ArrayList<>();
+
+        try {
+
+            Conexion con = new Conexion();
+            Connection cnx = con.objetenerConexion();
+
+            String query = "SELECT * FROM producto ";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                
+                Producto prt = new Producto();
+                prt.setCodigo(rs.getInt("codigo"));
+                prt.setPrecio(rs.getInt("precio"));
+                prt.setStock(rs.getInt("Stock"));
+                prt.setNombre(rs.getString("nombre"));
+                prt.setEditorial(rs.getString("auditorial"));
+                prt.setAutor(rs.getString("autor"));
+                
+                lista.add(prt);
+            }
+
+            rs.close();
+            stmt.close();
+            cnx.close();
+
+        } catch (HeadlessException | SQLException e) {
+            System.out.println("Error en la consulta SQL al consultar todos los datos" + e.getMessage());
+        }
+        return lista;
+
     }
     
 }
